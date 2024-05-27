2326cb98a09d6623c842650e18eb8aef20a3f223
congdat.nguyen <congdat.nguyen>
2024-05-27 18:14:11 +0700

    code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,261 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="w-full flex justify-center">
+    <img src="./assets/title.png" alt="" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="w-full md:flex md:justify-center">
+    <div class="w-full md:w-4/5" v-for="(img, index) in arrImg" :key="index">
+      <!-- <img src="./assets/title.png" alt="" /> -->
+      <img :src="img" alt="" v-if="showIndex == index" />
+    </div>
+  </div>
+  <div class="flex flex-col md:flex-row md:px-4">
+    <div class="flex justify-center md:justify-end">
+      <div class="text-center">
+        <p class="text-3xl md:text-7xl">Save the Date</p>
+        <p class="text-xl md:text-lg">For the weddiing of</p>
+        <p class="text-2xl md:text-4xl">Công Đạt & Thanh Hoa</p>
+        <p>
+          Thật vui vì được gặp và đón tiếp mọi trong một dịp đặc biệt như này.
+          Cảm ơn các mọi người rất nhiều vì sự hiện diện cùng những lời chúc tốt
+          đẹp mà mọi người đã dành cho gia đình và chúng em/anh/chị/cháu!
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-center md:justify-start mt-4">
+      <div class="w-full">
+        <div
+          class="text-center w-full"
+          v-for="(month, index) of month_names"
+          :key="index"
+        >
+          <div
+            v-if="index == 5"
+            class="p-1 m-1 font-sans bg-white w-full min-h-56 rounded shadow-md bg-blend-luminosity bg-gradient-to-b from-green-50 via-white to-green-50"
+          >
+            <p class="p-1 text-xl font-semibold text-center text-indigo-800">
+              {{ month }}-2024
+            </p>
+            <div class="p-1 m-1">
+              <div
+                class="grid grid-cols-7 font-semibold text-green-800 border-b-2"
+              >
+                <div v-for="days in day_names" :key="days">
+                  <div
+                    class="grid place-items-center"
+                    :class="{
+                      'text-red-600': days == 'CN',
+                    }"
+                  >
+                    <p class="text-2xl">{{ days }}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="grid grid-cols-7 gap-1 font-semibold text-center text-gray-800"
+              >
+                <div v-for="kun of daysgenerater()[index]" :key="kun">
+                  <div
+                    :class="{
+                      ' bg-pink-200 ring-4 rounded-full':
+                        isToDay(kun, index) == true,
+                      ' hover:bg-green-100': isToDay(kun, index) == false,
+                    }"
+                  >
+                    <p class="text-2xl">{{ kun }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col md:flex-row md:px-4 mt-4">
+    <div class="flex justify-center md:justify-end">
+      <div class="w-full text-center">
+        <div class="font-bold text-2xl">Tiệc nhà Trai</div>
+        <img src="/hoadat/src/assets/trai.jpg" alt="" />
+      </div>
+    </div>
+    <div class="flex justify-center md:justify-start mt-4">
+      <div class="w-full text-center">
+        <div class="font-bold text-2xl">Tiệc nhà Gái</div>
+        <img src="/hoadat/src/assets/gai.jpg" alt="" />
+      </div>
+    </div>
+  </div>
+  <div
+    v-for="flower in flowers"
+    :key="flower.id"
+    class="flower"
+    :style="{ left: flower.x + 'px', animationDuration: flower.duration + 's' }"
+  >
+    🌸
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.flower {
+  position: absolute;
+  top: -50px; /* Bắt đầu từ trên màn hình */
+  font-size: 14px;
+  animation-name: fall;
+  animation-timing-function: linear;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+@keyframes fall {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(100vh); /* Rơi xuống cuối màn hình */
+  }
 }
 </style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      arrImg: ["/hoadat/src/assets/3.JPG", "/hoadat/src/assets/2.JPG"],
+      month_names: [
+        "Tháng 1",
+        "Tháng 2",
+        "Tháng 3",
+        "Tháng 4",
+        "Tháng 5",
+        "Tháng 6",
+        "Tháng 7",
+        "Tháng 8",
+        "Tháng 9",
+        "Tháng 10",
+        "Tháng 11",
+        "Tháng 12",
+      ],
+      day_names: ["T2", "T3", "T4", "T5", "T6", "T7", "CN"],
+      flowers: [],
+      flowerId: 0,
+      showIndex: 0,
+    };
+  },
+  mounted() {
+    this.createFlowers();
+    this.startFlowerFall();
+  },
+  methods: {
+    createFlower() {
+      return {
+        id: this.flowerId++,
+        x: Math.random() * window.innerWidth,
+        duration: Math.random() * 5 + 10, // Thời gian rơi từ 5 đến 10 giây
+      };
+    },
+    createFlowers() {
+      for (let i = 0; i < 3; i++) {
+        // Số lượng hoa rơi ban đầu
+        this.flowers.push(this.createFlower());
+      }
+    },
+    startFlowerFall() {
+      setInterval(() => {
+        this.flowers.push(this.createFlower());
+        // Giới hạn số lượng hoa để không làm chậm trang
+        if (this.flowers.length > 100) {
+          this.flowers.shift();
+        }
+      }, 1000); // Tạo hoa mới mỗi 500ms
+      setInterval(() => {
+        this.showIndex++;
+        if (this.showIndex >= this.arrImg.length) {
+          this.showIndex = 0;
+        }
+      }, 2000); // chuyển slide mỗi 1s
+    },
+    namNhuan(year) {
+      return (
+        (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+        (year % 100 === 0 && year % 400 === 0)
+      );
+    },
+    daysNamNhuan(year) {
+      return this.namNhuan(year) ? 29 : 28;
+    },
+    daysgenerater() {
+      let days = [];
+      for (let k = 0; k < this.days_of_month().length; k++) {
+        days.push([]);
+        for (let i = 1; i <= this.days_of_month()[k]; i++) {
+          if (days[k].length < this.hafta(new Date().getFullYear(), k)) {
+            i -= i;
+            days[k].push("");
+            continue;
+          }
+          days[k].push(i);
+        }
+      }
+      return days;
+    },
+
+    days_of_month() {
+      return [
+        31,
+        this.daysNamNhuan(new Date().getFullYear()),
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+      ];
+    },
+    isToDay(kun, index) {
+      const today = new Date();
+      const dayintable = new Date(new Date().getFullYear(), index, kun);
+
+      return dayintable.getDate() == 1 || dayintable.getDate() == 2
+        ? true
+        : false;
+    },
+    hafta(sol, ma) {
+      let haftakuni = new Date(sol, ma).getDay();
+      switch (
+        haftakuni // hafta kuni Dushanbadan boshlangani uchun hak)
+      ) {
+        case 0:
+          haftakuni = 6;
+          break;
+        case 1:
+          haftakuni = 0;
+          break;
+        case 2:
+          haftakuni = 1;
+          break;
+        case 3:
+          haftakuni = 2;
+          break;
+        case 4:
+          haftakuni = 3;
+          break;
+        case 5:
+          haftakuni = 4;
+          break;
+        case 6:
+          haftakuni = 5;
+          break;
+        default:
+          haftakuni = new Date(sol, ma).getDay();
+          break;
+      }
+      return haftakuni;
+    },
+  },
+};
+</script>
